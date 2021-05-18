48c4da24cb07dbb452af1e91852e6c6aae2cbf3c
jukkty <dima.iachimov@gmail.com>
2021-05-18 11:16:28 +0300

    initial commit

---
+++ src/test/java/Tests.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+class Tests {
+    @Test
+    void shouldShowSomeData() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"));
+    }
+    @Test
+    void shouldShowHost() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("headers.host", equalTo ("postman-echo.com"));
+
+    }
+}
